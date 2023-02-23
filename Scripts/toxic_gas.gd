88f7c08b850dd953a27fa1cb46a705281e0e12cf
geodagame <grandrevolution69@gmail.com>
2023-02-23 16:47:34 -0300

    se sigue trabajando en una trampa

--- Scripts/toxic_gas.gd
+++ Scripts/toxic_gas.gd
@@ -1,10 +1,7 @@
 extends Area2D
 
-
-
-
-
 func _on_toxic_gas_body_entered(body):
-#	if body.name("Player"):
-#		body._velocity = 15
-	pass
+	if is_in_group("player"):
+#		Player._velocity.x = 10
+#		Player._velocity.y = 10
+		pass
